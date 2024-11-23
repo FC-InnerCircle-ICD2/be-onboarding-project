a1a58a004db5225309b1482911345af81d378803
cyeji <98408267+cyeji@users.noreply.github.com>
2024-11-23 17:03:26 +0900

    first commit

---
+++ src/test/java/com/innercircle/surveryproject/TestcontainersConfiguration.java
@@ -0,0 +1,8 @@
+package com.innercircle.surveryproject;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+}
