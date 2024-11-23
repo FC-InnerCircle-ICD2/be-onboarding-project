880f42a225dfcc5b866b133c0120badc2dd1bb8b
이승구 <leesg107@naver.com>
2024-11-23 17:06:59 +0900

    feat: first commit

---
+++ src/test/java/com/innercicle/beonboardingproject/BeOnboardingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innercicle.beonboardingproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeOnboardingProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
