196e5487f74b9f5f1cb71871223afbc5db81da09
cyeji <98408267+cyeji@users.noreply.github.com>
2024-11-24 21:38:04 +0900

    feat : 설문조사 등록 기능 추가

---
+++ src/main/java/com/innercircle/surveryproject/modules/dto/SurveyItemDto.java
@@ -0,0 +1,46 @@
+package com.innercircle.surveryproject.modules.dto;
+
+import com.innercircle.surveryproject.modules.entity.SurveyItem;
+import com.innercircle.surveryproject.modules.enums.ItemType;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 설문 받은 항목 dto
+ */
+@Data
+@NoArgsConstructor
+public class SurveyItemDto {
+
+    /**
+     * 이름
+     */
+    private String name;
+
+    /**
+     * 설명
+     */
+    private String description;
+
+    /**
+     * 항목 입력 형태
+     */
+    private ItemType itemType;
+
+    /**
+     * 필수여부
+     */
+    private Boolean required;
+
+    public SurveyItemDto(SurveyItem surveyItem) {
+        this.name = surveyItem.getName();
+        this.description = surveyItem.getDescription();
+        this.itemType = surveyItem.getItemType();
+        this.required = surveyItem.getRequired();
+    }
+
+    public static SurveyItemDto toDto(SurveyItem surveyItem) {
+        return new SurveyItemDto(surveyItem);
+    }
+
+}
---
+++ src/main/java/com/innercircle/surveryproject/modules/entity/Survey.java
@@ -0,0 +1,40 @@
+package com.innercircle.surveryproject.modules.entity;
+
+import com.innercircle.surveryproject.modules.dto.SurveyCreateDto;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Survey {
+
+    @Id @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Setter
+    @OneToMany(mappedBy = "survey")
+    private List<SurveyItem> surveyItemList;
+
+    public Survey(SurveyCreateDto surveyCreateDto) {
+        this.name = surveyCreateDto.getName();
+        this.description = surveyCreateDto.getDescription();
+    }
+
+    public static Survey from(SurveyCreateDto surveyCreateDto) {
+        return new Survey(surveyCreateDto);
+    }
+
+}
