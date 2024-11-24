25c59e41881c2cec7d7ee60af0707c778750d505
zephyr7419 <zephyr@musma.net>
2024-11-24 17:58:52 +0900

    feat: Add Core module with BaseEntity, common exception handling, and utilities
    <Description>
    - BaseEntity 추가
    - 날짜 포맷 유틸 추가
    - JPA Auditing 추가

--- core/src/main/java/com/onboarding/core/global/exception/CustomException.java
+++ core/src/main/java/com/onboarding/core/global/exception/CustomException.java
@@ -1,5 +1,16 @@
 package com.onboarding.core.global.exception;
 
+import com.onboarding.core.global.exception.enums.ErrorCode;
+
 public class CustomException extends RuntimeException{
-  public String message;
+  private final ErrorCode errorCode;
+
+  public CustomException(ErrorCode errorCode) {
+    super(errorCode.getMessage());
+    this.errorCode = errorCode;
+  }
+
+  public ErrorCode getErrorCode() {
+    return errorCode;
+  }
 }
