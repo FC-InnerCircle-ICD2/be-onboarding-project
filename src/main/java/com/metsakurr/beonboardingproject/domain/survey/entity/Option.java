82c9de7628c23f32d6bd0a3e2a8b27162cbd0872
youable <youable.framios@gmail.com>
2024-11-25 20:49:07 +0900

    Fix: 누락된 연관관계 수정

--- src/main/java/com/metsakurr/beonboardingproject/domain/survey/entity/Option.java
+++ src/main/java/com/metsakurr/beonboardingproject/domain/survey/entity/Option.java
@@ -1,16 +1,9 @@
 package com.metsakurr.beonboardingproject.domain.survey.entity;
 
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.Column;
 
 @Entity(name = "option")
 @Getter
@@ -26,5 +19,4 @@ public class Option {
 
     @Column(nullable = false)
     private String name;
-
 }
--- src/main/java/com/metsakurr/beonboardingproject/domain/survey/entity/Survey.java
+++ src/main/java/com/metsakurr/beonboardingproject/domain/survey/entity/Survey.java
@@ -1,12 +1,16 @@
 package com.metsakurr.beonboardingproject.domain.survey.entity;
 
 import com.metsakurr.beonboardingproject.common.entity.BaseEntity;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Column;
+import jakarta.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Comment;
+
+import java.util.List;
 
 @Entity(name = "survey")
 @Getter
@@ -19,4 +23,8 @@ public class Survey extends BaseEntity {
     @Comment("설문조사 설명")
     @Column(columnDefinition = "TEXT")
     private String description;
+
+    @OneToMany(mappedBy = "survey", cascade = CascadeType.ALL)
+    private List<Question> questions;
+
 }
