b072c2dc7c0ae235e350dc5262d8d346a634a4ba
brinst07 <brinst07@gmail.com>
2024-11-25 19:53:53 +0900

    first commit

---
+++ survey-common/src/test/java/org/brinst/surveycommon/SurveyCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package org.brinst.surveycommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveyCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
