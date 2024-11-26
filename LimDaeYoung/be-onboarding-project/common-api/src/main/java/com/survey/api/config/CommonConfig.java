4753cb6bdd7cb34e07b5a8333de42f9806dc6125
gkdl <limyoung92@naver.com>
2024-11-26 22:41:26 +0900

    Delete LimDaeYoung/be-onboarding-project directory
    실수햇습니다...

--- LimDaeYoung/be-onboarding-project/common-api/src/main/java/com/survey/api/config/CommonConfig.java
+++
@@ -1,8 +0,0 @@
-package com.survey.api.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class CommonConfig {
-}
