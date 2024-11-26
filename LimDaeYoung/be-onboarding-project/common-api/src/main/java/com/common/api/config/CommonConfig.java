0c6f17f7b5028e87f38214009f181dba5e8844b2
gkdl <limyoung92@naver.com>
2024-11-26 22:34:13 +0900

    설문조사 생성 및 수정 API 추가

--- LimDaeYoung/be-onboarding-project/common-api/src/main/java/com/common/api/config/CommonConfig.java
+++
@@ -1,9 +0,0 @@
-package com.common.api.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("com.common.api")
-public class CommonConfig {
-}
