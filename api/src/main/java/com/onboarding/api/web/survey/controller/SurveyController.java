ad1fe32f188ed0c11a2ea73db268e96b63affa5e
hyuk12 <hotman7025@naver.com>
2024-11-26 19:53:19 +0900

    fix: root 경로 src/main 제거 및 build.gradle 수정
    <description>
    - 기존에 root 경로에 main application 두었던 것 제거
    - root build.gradle 에서 모든 모듈을 관리하는 것에서 따로 따로 하는 것으로 변경

--- api/src/main/java/com/onboarding/api/web/survey/controller/SurveyController.java
+++ api/src/main/java/com/onboarding/api/web/survey/controller/SurveyController.java
@@ -4,11 +4,11 @@ import com.onboarding.api.web.survey.dto.CreateSurveyReqDto;
 import com.onboarding.survey.survey.facade.SurveyFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/api/v1/survey")
