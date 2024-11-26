268cf0be0b232ebcd807d788fb85eb7faf374870
ziwookim <ziwookiim@gmail.com>
2024-11-26 21:11:25 +0900

    feature. 프로젝트 생성

---
+++ src/main/java/ziwookim/be_onboarding_project/BeOnboardingProjectApplication.java
@@ -0,0 +1,13 @@
+package ziwookim.be_onboarding_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeOnboardingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeOnboardingProjectApplication.class, args);
+	}
+
+}
