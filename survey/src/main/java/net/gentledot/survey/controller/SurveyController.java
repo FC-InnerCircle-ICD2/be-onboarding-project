3193aea9b61d3f24be6afc5cc6608bd63d145289
gentledot <gentledot.wp@gmail.com>
2024-11-26 21:54:37 +0900

    feat(presentation layer 구성): 서베이 생성 요청 API 생성
    - ResponseEntity 추가

--- survey/src/main/java/net/gentledot/survey/controller/SurveyController.java
+++ survey/src/main/java/net/gentledot/survey/controller/SurveyController.java
@@ -4,6 +4,7 @@ import net.gentledot.survey.dto.common.ServiceResponse;
 import net.gentledot.survey.dto.request.SurveyCreateRequest;
 import net.gentledot.survey.dto.response.SurveyCreateResponse;
 import net.gentledot.survey.service.SurveyService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +19,7 @@ public class SurveyController {
     }
 
     @PostMapping
-    public ServiceResponse<SurveyCreateResponse> createSurvey(SurveyCreateRequest surveyRequest) {
-        return ServiceResponse.success(surveyService.createSurvey(surveyRequest));
+    public ResponseEntity<ServiceResponse<SurveyCreateResponse>> createSurvey(SurveyCreateRequest surveyRequest) {
+        return ResponseEntity.ok(ServiceResponse.success(surveyService.createSurvey(surveyRequest)));
     }
 }
