cb545a05454d9bcff4eece6bedb5392c721310a0
seungbin.kim <seungbin.kim.dev@gmail.com>
2024-11-26 23:23:55 +0900

    Test: 설문조사 생성 테스트코드 작성
    - 설문조사 생성 단위테스트 작성

---
+++ src/test/java/ic2/onboarding/survey/service/SurveyServiceUnitTest.java
@@ -0,0 +1,82 @@
+package ic2.onboarding.survey.service;
+
+import ic2.onboarding.survey.FixtureUtil;
+import ic2.onboarding.survey.dto.SurveyCreationRequest;
+import ic2.onboarding.survey.dto.SurveyCreationResponse;
+import ic2.onboarding.survey.dto.SurveyForm;
+import ic2.onboarding.survey.dto.SurveyFormItem;
+import ic2.onboarding.survey.entity.Survey;
+import ic2.onboarding.survey.entity.SurveyItem;
+import ic2.onboarding.survey.repository.SurveyRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class SurveyServiceUnitTest {
+
+    @Mock
+    private SurveyRepository surveyRepository;
+
+    @InjectMocks
+    private SurveyService surveyService;
+
+
+    @Test
+    @DisplayName("설문조사 생성 성공")
+    void createSurvey_success() {
+
+        // given
+        int size = 5;
+        List<String> choiceList = FixtureUtil.getDefaultFixtureMonkey()
+                .giveMeBuilder(String.class)
+                .set(FixtureUtil.getKoreanStringArbitrary())
+                .sampleList(3);
+
+        Survey survey = FixtureUtil.surveyArbitraryBuilder().sample();
+        List<SurveyItem> surveyItems = FixtureUtil.surveyItemArbitraryBuilder()
+                .set("choices", String.join("|", choiceList))
+                .sampleList(size);
+        survey.addAllItems(surveyItems);
+
+        when(surveyRepository.save(any(Survey.class)))
+                .thenReturn(survey);
+
+        SurveyForm basic = new SurveyForm(1L, survey.getName(), survey.getDescription());
+        List<SurveyFormItem> formItems = surveyItems.stream()
+                .map(SurveyFormItem::fromEntity)
+                .toList();
+
+        // when
+        SurveyCreationResponse response = surveyService.createSurvey(new SurveyCreationRequest(basic, formItems));
+
+        // then
+        SurveyForm surveyForm = response.basic();
+        List<SurveyFormItem> surveyFormItems = response.items();
+
+        assertEquals(survey.getName(), surveyForm.name());
+        assertEquals(survey.getDescription(), surveyForm.description());
+        assertEquals(surveyItems.size(), surveyFormItems.size());
+        for (int i = 0; i < surveyItems.size(); i++) {
+            SurveyItem surveyItem = surveyItems.get(i);
+            SurveyFormItem surveyFormItem = surveyFormItems.get(i);
+
+            assertEquals(surveyItem.getName(), surveyFormItem.name());
+            assertEquals(surveyItem.getDescription(), surveyFormItem.description());
+            assertEquals(surveyItem.getInputType().name(), surveyFormItem.inputType());
+            assertEquals(surveyItem.getRequired(), surveyFormItem.required());
+            assertEquals(surveyItem.getChoiceList(), surveyFormItem.choices());
+        }
+
+    }
+
+}
