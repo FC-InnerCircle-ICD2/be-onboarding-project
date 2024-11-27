21db56ca2484ab679e9abaacc0cf48a8d5632b94
gentledot <gentledot.wp@gmail.com>
2024-11-27 20:58:40 +0900

    fix(서베이 수정 API 구성): 서베이 수정 로직 확인 (2)
    - 서베이를 업데이트하면 기존 SurveyQuestion은 Survey = null 로 설정됨 확인.

--- survey/src/main/java/net/gentledot/survey/dto/request/SurveyUpdateRequest.java
+++ survey/src/main/java/net/gentledot/survey/dto/request/SurveyUpdateRequest.java
@@ -1,11 +1,13 @@
 package net.gentledot.survey.dto.request;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
 
 import java.util.List;
 
+@Builder
 @ToString
 @Getter
 @AllArgsConstructor
