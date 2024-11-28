fbad10fa21b0c503ac019a47f61d1348acb9ed96
youable <youable.framios@gmail.com>
2024-11-28 20:10:52 +0900

    Feat: CreateAnswerRequest validation 추가

--- src/main/java/com/metsakurr/beonboardingproject/domain/answer/dto/CreateAnswerRequest.java
+++ src/main/java/com/metsakurr/beonboardingproject/domain/answer/dto/CreateAnswerRequest.java
@@ -1,6 +1,7 @@
 package com.metsakurr.beonboardingproject.domain.answer.dto;
 
 import com.metsakurr.beonboardingproject.domain.answer.entity.Answer;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,15 +11,15 @@ import java.util.List;
 @Getter
 @NoArgsConstructor
 public class CreateAnswerRequest {
-    @NotBlank(message = "idx[응답할 설문 조사 식별자]가 필요합니다.")
-    private long idx;
+    @Min(value = 1, message = "유효한 idx[응답할 설문 조사 식별자]가 필요합니다.")
+    private int idx;
 
     private List<AnswerRequest> answers;
 
     @Getter
     public static class AnswerRequest {
-        @NotBlank(message = "idx[항목 식별자]가 필요합니다.")
-        private long idx;
+        @Min(value = 1, message = "유효한 idx[항목 식별자]가 필요합니다.")
+        private int idx;
 
         private String answer;
     }
