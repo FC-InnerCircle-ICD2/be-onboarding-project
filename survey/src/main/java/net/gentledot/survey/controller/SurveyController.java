fd5de4f8995ec7097e4e1a27c1ab5abb85ee03b3
gentledot <gentledot.wp@gmail.com>
2024-11-28 23:14:55 +0900

    feat(서베이 응답 조회 API 구성): api 추가

--- survey/src/main/java/net/gentledot/survey/controller/SurveyController.java
+++ survey/src/main/java/net/gentledot/survey/controller/SurveyController.java
@@ -1,14 +1,17 @@
 package net.gentledot.survey.controller;
 
 import net.gentledot.survey.dto.common.ServiceResponse;
+import net.gentledot.survey.dto.request.SearchSurveyAnswerRequest;
 import net.gentledot.survey.dto.request.SubmitSurveyAnswer;
 import net.gentledot.survey.dto.request.SurveyCreateRequest;
 import net.gentledot.survey.dto.request.SurveyUpdateRequest;
+import net.gentledot.survey.dto.response.SearchSurveyAnswerResponse;
 import net.gentledot.survey.dto.response.SurveyCreateResponse;
 import net.gentledot.survey.dto.response.SurveyUpdateResponse;
 import net.gentledot.survey.service.SurveyAnswerService;
 import net.gentledot.survey.service.SurveyService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -32,13 +35,15 @@ public class SurveyController {
     @PostMapping
     public ResponseEntity<ServiceResponse<SurveyCreateResponse>> createSurvey(
             @RequestBody SurveyCreateRequest surveyRequest) {
-        ServiceResponse<SurveyCreateResponse> createdResult = ServiceResponse.success(surveyService.createSurvey(surveyRequest));
-        return ResponseEntity.ok(createdResult);
+        SurveyCreateResponse createResult = surveyService.createSurvey(surveyRequest);
+        return ResponseEntity.ok(ServiceResponse.success(createResult));
     }
 
     @PutMapping
-    public ResponseEntity<ServiceResponse<SurveyUpdateResponse>> updateSurvey(SurveyUpdateRequest surveyRequest) {
-        return ResponseEntity.ok(ServiceResponse.success(surveyService.updateSurvey(surveyRequest)));
+    public ResponseEntity<ServiceResponse<SurveyUpdateResponse>> updateSurvey(
+            @RequestBody SurveyUpdateRequest surveyRequest) {
+        SurveyUpdateResponse updateResult = surveyService.updateSurvey(surveyRequest);
+        return ResponseEntity.ok(ServiceResponse.success(updateResult));
     }
 
     @PostMapping("/{surveyId}/answer")
@@ -46,5 +51,12 @@ public class SurveyController {
                                                                     @RequestBody List<SubmitSurveyAnswer> answer) {
         surveyAnswerService.submitSurveyAnswer(surveyId, answer);
         return ResponseEntity.ok(ServiceResponse.success(null));
+    }
+
+    @GetMapping("/{surveyId}/answer/all")
+    public ResponseEntity<ServiceResponse<SearchSurveyAnswerResponse>> getAllSurveyAnswers(@PathVariable("surveyId") String surveyId,
+                                                                                           @RequestBody SearchSurveyAnswerRequest request) {
+        SearchSurveyAnswerResponse surveyAnswers = surveyAnswerService.getSurveyAnswers(request);
+        return ResponseEntity.ok(ServiceResponse.success(surveyAnswers));
     }
 }
