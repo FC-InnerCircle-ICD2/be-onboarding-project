aec52127b3623713d32b27b21744fb163b29d789
youable <youable.framios@gmail.com>
2024-11-28 17:45:13 +0900

    Refactor: Option, Question관련 JpaRepository에서 QueryDSL로 변경

--- src/main/java/com/metsakurr/beonboardingproject/domain/survey/repository/QuestionRepository.java
+++ src/main/java/com/metsakurr/beonboardingproject/domain/survey/repository/QuestionRepository.java
@@ -1,9 +1,30 @@
 package com.metsakurr.beonboardingproject.domain.survey.repository;
 
+import com.metsakurr.beonboardingproject.domain.survey.entity.QQuestion;
 import com.metsakurr.beonboardingproject.domain.survey.entity.Question;
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
-public interface QuestionRepository extends JpaRepository<Question, Long> {
+@RequiredArgsConstructor
+public class QuestionRepository {
+    private final EntityManager entityManager;
+    private final JPAQueryFactory queryFactory;
+
+    @Transactional
+    public void save(Question question) {
+        entityManager.persist(question);
+    }
+
+    public List<Question> findAllBySurveyId(long surveyId) {
+        QQuestion question = QQuestion.question;
+        return queryFactory.selectFrom(question)
+                .where(question.survey.idx.eq(surveyId))
+                .fetch();
+    }
 }
