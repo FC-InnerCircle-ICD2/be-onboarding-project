a521f399f1c9c9418442031749e855c3966f8455
gentledot <gentledot.wp@gmail.com>
2024-11-30 09:40:37 +0900

    docs(서베이 기능 요구사항 동작점검): SurveyIntegrationTest 작성 (4) - 생성 기능
    - ValidatableResponse 별도 보관

--- survey/src/test/java/net/gentledot/survey/controller/SurveyIntegrationTest.java
+++ survey/src/test/java/net/gentledot/survey/controller/SurveyIntegrationTest.java
@@ -1,16 +1,20 @@
 package net.gentledot.survey.controller;
 
 import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 
+import static org.hamcrest.Matchers.emptyString;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.nullValue;
 
+@Slf4j
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SurveyIntegrationTest {
 
@@ -24,68 +28,65 @@ class SurveyIntegrationTest {
 
     @Test
     void createSurvey() {
-        RestAssured.given()
-                .contentType(ContentType.JSON)
-                .body(
-                        """
-                                {
-                                    "name": "나만의 설문",
-                                    "description": "간단한 설문입니다.",
-                                    "questions": [
-                                      {
-                                        "question": "이름을 알려주세요",
-                                        "description": "당신의 이름은 무엇입니까?",
-                                        "type": "TEXT",
-                                        "required": "REQUIRED"
-                                
-                                      },
-                                      {
-                                        "question": "오늘의 기분을 알려주세요",
-                                        "description": "가벼운 질문부터",
-                                        "type": "SINGLE_SELECT",
-                                        "required": "REQUIRED",
-                                        "options": [
-                                          {
-                                            "option": "좋아요"
-                                          },
-                                          {
-                                            "option": "안좋아요"
-                                          }
-                                        ]
-                                      },
-                                      {
-                                        "question": "잠에 드는 시간대가 언제 즈음인가요?",
-                                        "description": "23시에서 01시까지",
-                                        "type": "MULTI_SELECT",
-                                        "required": "REQUIRED",
-                                        "options": [
-                                          {
-                                            "option": "23:00"
-                                          },
-                                          {
-                                            "option": "24:00"
-                                          },
-                                          {
-                                            "option": "01:00"
-                                          }
-                                        ]
-                                      },
-                                      {
-                                        "question": "설문이 어떠셨는지 의견을 남겨주세요",
-                                        "description": "간단한 의견이라도 좋습니다. :)",
-                                        "type": "PARAGRAPH",
-                                        "required": "OPTIONAL"                                                                                                                                                  
-                                      }                                      
-                                    ]                                                
-                                 }
-                                """
-                )
-                .when()
-                .post("/v1/survey")
-                .then()
+        String requestBody = """
+                {
+                    "name": "나만의 설문",
+                    "description": "간단한 설문입니다.",
+                    "questions": [
+                      {
+                        "question": "이름을 알려주세요",
+                        "description": "당신의 이름은 무엇입니까?",
+                        "type": "TEXT",
+                        "required": "REQUIRED"
+                
+                      },
+                      {
+                        "question": "오늘의 기분을 알려주세요",
+                        "description": "가벼운 질문부터",
+                        "type": "SINGLE_SELECT",
+                        "required": "REQUIRED",
+                        "options": [
+                          {
+                            "option": "좋아요"
+                          },
+                          {
+                            "option": "안좋아요"
+                          }
+                        ]
+                      },
+                      {
+                        "question": "잠에 드는 시간대가 언제 즈음인가요?",
+                        "description": "23시에서 01시까지",
+                        "type": "MULTI_SELECT",
+                        "required": "REQUIRED",
+                        "options": [
+                          {
+                            "option": "23:00"
+                          },
+                          {
+                            "option": "24:00"
+                          },
+                          {
+                            "option": "01:00"
+                          }
+                        ]
+                      },
+                      {
+                        "question": "설문이 어떠셨는지 의견을 남겨주세요",
+                        "description": "간단한 의견이라도 좋습니다. :)",
+                        "type": "PARAGRAPH",
+                        "required": "OPTIONAL"
+                      }
+                    ]
+                 }
+                """;
+        var response = SurveyIntegrations.surveyCreate(requestBody);
+        response
                 .statusCode(HttpStatus.OK.value())
                 .body("success", equalTo(true))
-                .body("data", notNullValue())
-                .log().all();
+                .body("data.surveyId", not(emptyString()))
+                .body("data.createdAt", notNullValue())
+                .body("error", nullValue());
+
     }
 }
---
+++ survey/src/test/java/net/gentledot/survey/controller/SurveyIntegrations.java
@@ -0,0 +1,23 @@
+package net.gentledot.survey.controller;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.ValidatableResponse;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class SurveyIntegrations {
+    public static ValidatableResponse surveyCreate(String requestBody) {
+
+        return RestAssured.given()
+                .contentType(ContentType.JSON)
+                .body(
+                        requestBody
+                )
+                .when()
+                .post("/v1/survey")
+                .then()
+                .log().all();
+    }
+}
