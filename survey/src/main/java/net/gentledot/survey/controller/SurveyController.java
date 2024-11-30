397d9d7df45c11e4ba489e4c2cd2f1733fc07b3a
gentledot <gentledot.wp@gmail.com>
2024-11-30 17:00:11 +0900

    docs(api documentation): swagger (openApi) 추가
    - SurveyController에 우선 적용

--- survey/src/main/java/net/gentledot/survey/controller/SurveyController.java
+++ survey/src/main/java/net/gentledot/survey/controller/SurveyController.java
@@ -1,5 +1,8 @@
 package net.gentledot.survey.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import net.gentledot.survey.dto.common.ServiceResponse;
 import net.gentledot.survey.dto.request.SearchSurveyAnswerRequest;
 import net.gentledot.survey.dto.request.SubmitSurveyAnswer;
@@ -22,6 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Tag(name = "Survey", description = "설문조사 서비스 API")
 @RequestMapping("v1/survey")
 @RestController
 public class SurveyController {
@@ -33,6 +37,7 @@ public class SurveyController {
         this.surveyAnswerService = surveyAnswerService;
     }
 
+    @Operation(summary = "설문조사 생성", description = "새로운 설문조사를 생성합니다.")
     @PostMapping
     public ResponseEntity<ServiceResponse<SurveyCreateResponse>> createSurvey(
             @RequestBody SurveyCreateRequest surveyRequest) {
@@ -40,6 +45,7 @@ public class SurveyController {
         return ResponseEntity.ok(ServiceResponse.success(createResult));
     }
 
+    @Operation(summary = "설문조사 수정", description = "생성된 설문조사의 내용을 수정합니다.")
     @PutMapping
     public ResponseEntity<ServiceResponse<SurveyUpdateResponse>> updateSurvey(
             @RequestBody SurveyUpdateRequest surveyRequest) {
@@ -47,17 +53,22 @@ public class SurveyController {
         return ResponseEntity.ok(ServiceResponse.success(updateResult));
     }
 
+    @Operation(summary = "설문조사 응답 제출", description = "대상 설문조사에 응답을 제출합니다.")
     @PostMapping("/{surveyId}/answer")
-    public ResponseEntity<ServiceResponse<Void>> submitSurveyAnswer(@PathVariable("surveyId") String surveyId,
+    public ResponseEntity<ServiceResponse<Void>> submitSurveyAnswer(
+            @Parameter(description = "설문조사 ID", required = true) @PathVariable("surveyId") String surveyId,
                                                                     @RequestBody List<SubmitSurveyAnswer> answer) {
         surveyAnswerService.submitSurveyAnswer(surveyId, answer);
         return ResponseEntity.ok(ServiceResponse.success(null));
     }
 
+
+    @Operation(summary = "설문조사 응답 조회", description = "대상 설문조사에 제출된 응답을 조회합니다.")
     @GetMapping("/{surveyId}/answer/all")
-    public ResponseEntity<ServiceResponse<SearchSurveyAnswerResponse>> getAllSurveyAnswersWithQuery(@PathVariable("surveyId") String surveyId,
-                                                                                                    @RequestParam(value = "questionName", required = false) String questionName,
-                                                                                                    @RequestParam(value = "answer", required = false) String answerValue) {
+    public ResponseEntity<ServiceResponse<SearchSurveyAnswerResponse>> getAllSurveyAnswersWithQuery(
+            @Parameter(description = "설문조사 ID", required = true) @PathVariable("surveyId") String surveyId,
+            @Parameter(description = "질문 이름", required = false) @RequestParam(value = "questionName", required = false) String questionName,
+            @Parameter(description = "응답 값", required = false) @RequestParam(value = "answer", required = false) String answerValue) {
         SearchSurveyAnswerRequest request = SearchSurveyAnswerRequest.fromRequest(surveyId, questionName, answerValue);
         SearchSurveyAnswerResponse surveyAnswers = surveyAnswerService.getSurveyAnswers(request);
         return ResponseEntity.ok(ServiceResponse.success(surveyAnswers));
