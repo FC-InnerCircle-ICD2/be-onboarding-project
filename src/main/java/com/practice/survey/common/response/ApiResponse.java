8214e5d9dd9b88a292f46d3ae9a6302d916ce460
Carnival77 <stam0325@gmail.com>
2024-11-30 10:54:03 +0900

    refactor : 서버 에러 응답용 메소드 추가

--- src/main/java/com/practice/survey/common/response/ApiResponse.java
+++ src/main/java/com/practice/survey/common/response/ApiResponse.java
@@ -31,4 +31,18 @@ public class ApiResponse<T> {
                 .common(Common.builder().build().responseOk())
                 .build();
     }
+
+    @JsonIgnore
+    public ApiResponse<T> serverError(StatusEnum status) {
+        return ApiResponse.<T>builder()
+                .common(Common.builder().build().serverError(status))
+                .build();
+    }
+
+    @JsonIgnore
+    public ApiResponse<T> serverError(int code, String message) {
+        return ApiResponse.<T>builder()
+                .common(Common.builder().build().serverError(code, message))
+                .build();
+    }
 }
