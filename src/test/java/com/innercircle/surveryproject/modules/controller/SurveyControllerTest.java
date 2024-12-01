5e752ed2610b60b2e0632933a6678d4a28a3cdf0
cyeji <98408267+cyeji@users.noreply.github.com>
2024-12-01 22:33:06 +0900

    test : 설문조사 수정 성공 테스트

--- src/test/java/com/innercircle/surveryproject/modules/controller/SurveyControllerTest.java
+++ src/test/java/com/innercircle/surveryproject/modules/controller/SurveyControllerTest.java
@@ -8,9 +8,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.test.web.servlet.ResultActions;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -20,8 +21,6 @@ class SurveyControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
-
-    private ObjectMapper objectMapper = new ObjectMapper();
 
     @Test
     @DisplayName("설문조사 등록 시 설문조사 항목이 없는 경우 에러 발생")
@@ -46,6 +45,24 @@ class SurveyControllerTest {
                 status().isCreated())
             .andExpect(jsonPath("$.message").value("설문조사 등록에 성공하였습니다."));
 
+    }
+
+    @Test
+    @DisplayName("설문조사 수정 성공")
+    void test_case_3() throws Exception {
+        // given
+        String request = FileUtils.readFileAsString("testcase/survey_update_success.txt");
+        // when // then
+        ResultActions result = mockMvc.perform(put("/api/survey")
+                                                   .contentType(MediaType.APPLICATION_JSON)
+                                                   .content(request));
+
+        // 응답 검증
+        result.andExpect(status().isOk())
+            .andExpect(jsonPath("$.status").value(200))
+            .andExpect(jsonPath("$.message").value("설문조사 수정에 성공하였습니다."))
+            .andExpect(jsonPath("$.data.name").value("서비스 만족도 설문조사"))
+            .andExpect(jsonPath("$.data.description").value("서비스 만족도를 평가하는 설문조사입니다."));
     }
 
 }
