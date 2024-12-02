a400b8d99d283bd9ede49e05815ac9bcabb7aeda
Carnival77 <stam0325@gmail.com>
2024-12-02 21:25:54 +0900

    feat : 유효성 검사 및 글로벌 에러 핸들러 기능 구현

---
+++ src/main/java/com/practice/survey/common/handler/ValidationException.java
@@ -0,0 +1,18 @@
+package com.practice.survey.common.handler;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ValidationException {
+
+    private int code;
+
+    private String message;
+
+}
