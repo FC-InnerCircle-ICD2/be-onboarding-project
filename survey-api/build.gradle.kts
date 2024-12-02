99995826c368deb617937ed2a9c1390c2ea41717
yesjm <yesjm.dev@gmail.com>
2024-12-02 20:14:13 +0900

    fix: asciidoc 설정 수정

--- survey-api/build.gradle.kts
+++ survey-api/build.gradle.kts
@@ -4,9 +4,15 @@ plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
     kotlin("plugin.jpa") version "1.9.25"
+    id("org.asciidoctor.jvm.convert") version "3.3.2"
 }
 
 extra["snippetsDir"] = file("build/generated-snippets")
+
+val asciidoctorExt = "asciidoctorExt"
+configurations.create("asciidoctorExt") {
+    extendsFrom(configurations.testImplementation.get())
+}
 
 dependencies {
     implementation("com.h2database:h2")
@@ -17,21 +23,27 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-data-jpa")
     testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
     testImplementation("org.mockito.kotlin:mockito-kotlin:5.3.1")
+    asciidoctorExt("org.springframework.restdocs:spring-restdocs-asciidoctor")
 }
 
-tasks.bootJar {
-    enabled = false
-}
+val snippetsDir by extra { file("build/generated-snippets") }
 
-tasks.jar {
-    enabled = true
-}
+tasks {
+    test {
+        outputs.dir(snippetsDir)
+        useJUnitPlatform()
+    }
 
-tasks.test {
-    outputs.dir(rootProject.extra["snippetsDir"] ?: "build/generated-snippets")
-}
+    asciidoctor {
+        inputs.dir(snippetsDir)
+        configurations("asciidoctorExt")
+        dependsOn(test)
+    }
 
-tasks.asciidoctor {
-    inputs.dir(rootProject.extra["snippetsDir"] ?: "build/generated-snippets")
-    dependsOn(tasks.test)
+    bootJar {
+        dependsOn(asciidoctor)
+        from ("build/docs/asciidoc") {
+            into("static/docs")
+        }
+    }
 }
