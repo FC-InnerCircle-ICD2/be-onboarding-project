86602a8be95ba219a28c5ce3ed25a911f316aa10
InvestLee <dlwlgud216@nate.com>
2024-12-03 20:01:39 +0900

    설문조사 기능에 따른 주석 추가

--- api/src/main/java/org/survey/api/domain/survey/business/SurveyBusiness.java
+++ api/src/main/java/org/survey/api/domain/survey/business/SurveyBusiness.java
@@ -26,14 +26,26 @@ public class SurveyBusiness {
     private final SurveyService surveyService;
     private final SurveyConverter surveyConverter;
 
+    /**
+     * 설문조사 생성
+     * 1. 설문조사 폼 등록
+     * 2. 설문조사 항목 등록
+     * 3. 항목 입력 형태가 [단일선택리스트], [다중선택리스트]의 경우 선택 리스트 등록
+     * 4. 설문조사 등록 결과 return
+     */
     @Transactional
     public SurveyBaseResponse register(SurveyBaseRequest request){
+        //설문조사 폼 등록
         var baseEntity = surveyConverter.toEntity(request);
         var newBaseEntity = surveyService.baseRegister(baseEntity);
+
+        //설문조사 항목 등록
         List<SurveyItemResponse> items = new ArrayList<>();
         for(SurveyItemRequest item : request.getItems()){
             var itemEntity = surveyConverter.toEntity(item, newBaseEntity.getId());
             var newItemEntity = surveyService.itemRegister(itemEntity);
+
+            //항목 입력 형태가 [단일선택리스트], [다중선택리스트]의 경우 선택 리스트 등록
             List<SelectOptionResponse> selectOptionResponseList = new ArrayList<>();
             if(newItemEntity.getInputType() == ItemInputType.SINGLE_SELECT_LIST
                     || newItemEntity.getInputType() == ItemInputType.MULTI_SELECT_LIST){
@@ -46,17 +58,32 @@ public class SurveyBusiness {
                     selectOptionResponseList.add(surveyConverter.toResponse(newSelectListEntity));
                 }
             }
+
+            //설문조사 항목 Response 변환
             var itemResponse = surveyConverter.toResponse(newItemEntity, selectOptionResponseList);
             items.add(itemResponse);
         }
+        //설문조사 등록 결과 return
         var response = surveyConverter.toResponse(newBaseEntity, items);
         return response;
     }
 
+    /**
+     * 식별자를 통한 설문조사 검색
+     * 1. 설문조사 폼 Select
+     * 2. 설문조사 항목 List Select
+     * 3. 항목 입력 형태가 [단일선택리스트], [다중선택리스트]의 경우 선택 리스트 Select
+     * 4. 설문조사 검색 결과 return
+     */
     @Transactional
     public SurveyBaseResponse find(Long id){
+        //설문조사 폼 Select
         var baseEntity = surveyService.baseFindById(id);
+
+        //설문조사 항목 List Select
         var itemEntityList = surveyService.itemAllFind(baseEntity.getId());
+
+        //항목 입력 형태가 [단일선택리스트], [다중선택리스트]의 경우 선택 리스트 Select
         List<SurveyItemResponse> itemResponseList = new ArrayList<>();
         for(SurveyItemEntity itemEntity : itemEntityList){
             List<SelectOptionResponse> selectOptionList = new ArrayList<>();
@@ -73,21 +100,37 @@ public class SurveyBusiness {
             var itemResponse = surveyConverter.toResponse(itemEntity, selectOptionList);
             itemResponseList.add(itemResponse);
         }
+        
+        //설문조사 검색 결과 return
         var response = surveyConverter.toResponse(baseEntity, itemResponseList);
         return response;
     }
 
+    /**
+     * 설문조사 수정
+     * 1. 설문조사 폼 수정
+     * 2. 설문조사 항목 수정
+     * 3. 항목 입력 형태가 [단일선택리스트], [다중선택리스트]의 경우 선택 리스트 수정
+     * 4. 항목 입력 형태가 [단일선택리스트], [다중선택리스트]의 경우 불필요 선택 리스트 삭제
+     * 5. 불필요 설문조사 항목 삭제
+     * 6. 설문조사 수정 결과 return
+     */
     @Transactional
     public SurveyBaseResponse updateAll(
             SurveyBaseRequest request
     ){
+        //설문조사 폼 수정
         var baseEntity = surveyConverter.toEntity(request);
         var newBaseEntity = surveyService.baseUpdate(request.getId(), baseEntity);
+
+        //설문조사 항목 수정
         List<SurveyItemResponse> items = new ArrayList<>();
         HashSet<Long> itemIdList = new HashSet<>();
         for(SurveyItemRequest item : request.getItems()){
             var itemEntity = surveyConverter.toEntity(item, newBaseEntity.getId());
             var newItemEntity = surveyService.itemUpdate(itemEntity, newBaseEntity.getId());
+
+            //항목 입력 형태가 [단일선택리스트], [다중선택리스트]의 경우 선택 리스트 수정
             List<SelectOptionResponse> selectOptionResponseList = new ArrayList<>();
             if(newItemEntity.getInputType() == ItemInputType.SINGLE_SELECT_LIST
                     || newItemEntity.getInputType() == ItemInputType.MULTI_SELECT_LIST){
@@ -105,6 +148,8 @@ public class SurveyBusiness {
                     selectOptionResponseList.add(optionResponse);
                     optionIdList.add(optionResponse.getId());
                 }
+
+                //불필요 선택 리스트 삭제
                 List<SelectListEntity> currentSelectList = surveyService.selectListAllFind(
                         newBaseEntity.getId(),
                         newItemEntity.getId());
@@ -117,10 +162,14 @@ public class SurveyBusiness {
                     }
                 }
             }
+
+            //설문조사 항목 Response 변환
             var itemResponse = surveyConverter.toResponse(newItemEntity, selectOptionResponseList);
             items.add(itemResponse);
             itemIdList.add(itemResponse.getId());
         }
+
+        //불필요 설문조사 항목 삭제
         List<SurveyItemEntity> currentItemList = surveyService.itemAllFind(newBaseEntity.getId());
         for(SurveyItemEntity currentItem : currentItemList){
             if(!itemIdList.contains(currentItem.getId())){
@@ -129,17 +178,32 @@ public class SurveyBusiness {
                         currentItem.getSurveyId());
             }
         }
+        
+        //설문조사 수정 결과 return
         var response = surveyConverter.toResponse(newBaseEntity, items);
         return response;
     }
 
+    /**
+     * 설문조사 응답 제출
+     * 1. 유효한 설문조사 폼인지 판별
+     * 2. 설문조사 항목 리스트 Select
+     * 3. 항목 입력 형태가 [단일선택리스트], [다중선택리스트]의 경우 유효한 후보 선택 여부 판별
+     * 4. 항목 입력 형태가 [다중선택리스트]가 아닌 경우 답변 중복 여부 판별
+     * 5. 설문조사 응답 결과 return
+     */
     @Transactional
     public SurveyReplyListResponse reply(SurveyReplyListRequest surveyReplyListRequest){
+        //유효한 설문조사 폼인지 판별 
         SurveyBaseEntity surveyBaseEntity = surveyService.baseFindById(surveyReplyListRequest.getId());
+
+        //설문조사 항목 리스트 Select
         List<SurveyReplyResponse> replies = new ArrayList<>();
         for(SurveyReplyRequest surveyReplyRequest : surveyReplyListRequest.getReply()){
             var surveyReplyEntity = surveyConverter.toEntity(surveyReplyRequest, surveyReplyListRequest.getId());
             var surveyItem = surveyService.itemFindById(surveyReplyEntity.getItemId(), surveyReplyListRequest.getId());
+
+            //항목 입력 형태가 [단일선택리스트], [다중선택리스트]의 경우 유효한 후보 선택 여부 판별
             if(surveyItem.getInputType() == ItemInputType.SINGLE_SELECT_LIST
             || surveyItem.getInputType() == ItemInputType.MULTI_SELECT_LIST){
                 List<SelectListEntity> selectListEntityList = surveyService.selectListAllFind(
@@ -152,6 +216,8 @@ public class SurveyBusiness {
                     throw new ApiException(SelectErrorCode.NON_EXIST_REQUEST, "The request does not exist in the selection list.");
                 }
             }
+
+            //항목 입력 형태가 [다중선택리스트]가 아닌 경우 답변 중복 여부 판별
             if(!(surveyItem.getInputType() == ItemInputType.MULTI_SELECT_LIST)){
                 List<SurveyReplyEntity> surveyReplyEntityList = surveyService.replyMultiFind(
                         surveyReplyListRequest.getId(), surveyReplyEntity.getItemId());
@@ -159,20 +225,30 @@ public class SurveyBusiness {
                     throw new ApiException(SelectErrorCode.DUPLICATE_REQUEST, "This is a duplicate request");
                 }
             }
+
+            //설문조사 응답 등록
             var newSurveyReplyEntity = surveyService.replyRegister(surveyReplyEntity);
             var surveyReplyResponse = surveyConverter.toResponse(newSurveyReplyEntity);
             replies.add(surveyReplyResponse);
         }
+
+        //설문조사 응답 결과 return
         var response = new SurveyReplyListResponse(surveyReplyListRequest.getId(), replies);
         return response;
     }
 
+    /**
+     * 모든 설문조사 List Select
+     */
     @Transactional
     public List<SurveyListResponse> baseAllFind(){
         var baseEntityList = surveyService.baseAllFind();
         return surveyConverter.toBaseListResponse(baseEntityList);
     }
 
+    /**
+     * 설문조사 응답 조회
+     */
     @Transactional
     public SurveyReplyListResponse replyAllFind(Long id){
         var replyEntityList = surveyService.replyAllFind(id);
@@ -181,6 +257,9 @@ public class SurveyBusiness {
         return response;
     }
 
+    /**
+     * 설문조사 삭제
+     */
     @Transactional
     public SurveyListResponse deleteSurvey(Long id){
         var baseEntity = surveyService.baseDelete(id);
