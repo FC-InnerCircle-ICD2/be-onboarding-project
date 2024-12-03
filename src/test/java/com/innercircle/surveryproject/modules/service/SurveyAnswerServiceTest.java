78dae9d2aa5899da9b296fdcd23a17805748b605
cyeji <98408267+cyeji@users.noreply.github.com>
2024-12-03 19:57:39 +0900

    test : 설문조사 응답 제출 성공 테스트 추가

--- src/test/java/com/innercircle/surveryproject/modules/service/SurveyAnswerServiceTest.java
+++ src/test/java/com/innercircle/surveryproject/modules/service/SurveyAnswerServiceTest.java
@@ -3,23 +3,24 @@ package com.innercircle.surveryproject.modules.service;
 import com.innercircle.surveryproject.global.utils.FileUtils;
 import com.innercircle.surveryproject.infra.exceptions.InvalidInputException;
 import com.innercircle.surveryproject.modules.dto.SurveyAnswerCreateDto;
-import com.innercircle.surveryproject.modules.entity.Survey;
+import com.innercircle.surveryproject.modules.dto.SurveyAnswerDto;
 import com.innercircle.surveryproject.modules.repository.SurveyRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
+import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.shaded.com.fasterxml.jackson.databind.JsonNode;
 import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-@Sql(scripts = "/data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Transactional
+@Sql(scripts = "/data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_CLASS)
 @SpringBootTest
 class SurveyAnswerServiceTest {
 
@@ -34,12 +35,26 @@ class SurveyAnswerServiceTest {
     @Test
     @DisplayName("설문조사 응답 생성 시 필수 항목이 누락된 경우 에러")
     void testSurveyAnswer() throws IOException {
-        List<Survey> all = surveyRepository.findAll();
-        System.out.println(all);
         JsonNode jsonNode = FileUtils.readFileAsJson("testcase/survey_answer_testcase1.txt");
         SurveyAnswerCreateDto surveyAnswerCreateDto = objectMapper.treeToValue(jsonNode, SurveyAnswerCreateDto.class);
-        String message = assertThrows(InvalidInputException.class, () -> surveyAnswerService.createSurveyAnswer(surveyAnswerCreateDto)).getMessage();
+        String message =
+            assertThrows(InvalidInputException.class, () -> surveyAnswerService.createSurveyAnswer(surveyAnswerCreateDto)).getMessage();
         assertEquals("필수 항목을 입력해주세요.", message);
+    }
+
+    @Test
+    @DisplayName("설문조사 응답 제출 시 저장 성공")
+    void test_case_1() throws Exception {
+        // given
+        JsonNode jsonNode = FileUtils.readFileAsJson("testcase/survey_answer_testcase2.txt");
+        SurveyAnswerCreateDto surveyAnswerCreateDto = objectMapper.treeToValue(jsonNode, SurveyAnswerCreateDto.class);
+        // when // then
+        SurveyAnswerDto surveyAnswer = surveyAnswerService.createSurveyAnswer(surveyAnswerCreateDto);
+
+        assertEquals(8201049505032L, surveyAnswer.getPhoneNumber());
+        assertEquals("조예지", surveyAnswer.getUsername());
+        assertEquals("만족", surveyAnswer.getSurveyAnswerMap().get(1L));
+        assertEquals("테스트", surveyAnswer.getSurveyAnswerMap().get(2L));
     }
 
 }
