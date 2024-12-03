d289fdb7ca7649e1665b80e230c70f54d96e07e8
cyeji <98408267+cyeji@users.noreply.github.com>
2024-12-03 19:56:59 +0900

    fix : 데이터 생성 방식 변경
    : 메소드 호출 및 BeforeEach대신 sql데이터 삽입 방식으로 변경

--- src/test/java/com/innercircle/surveryproject/modules/controller/SurveyUpdateTest.java
+++ src/test/java/com/innercircle/surveryproject/modules/controller/SurveyUpdateTest.java
@@ -1,28 +1,21 @@
 package com.innercircle.surveryproject.modules.controller;
 
 import com.innercircle.surveryproject.global.utils.FileUtils;
-import com.innercircle.surveryproject.modules.entity.Survey;
-import com.innercircle.surveryproject.modules.repository.SurveyRepository;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
-import org.testcontainers.shaded.com.fasterxml.jackson.databind.JsonNode;
-import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.io.IOException;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@Transactional
 @SpringBootTest
 @AutoConfigureMockMvc
 class SurveyUpdateTest {
@@ -30,25 +23,11 @@ class SurveyUpdateTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
-    private SurveyRepository surveyRepository;
-
-    private ObjectMapper objectMapper = new ObjectMapper();
-
-    private void setup() throws IOException {
-        JsonNode jsonNode = FileUtils.readFileAsJson("testcase/success_survey.txt");
-        Survey survey = objectMapper.treeToValue(jsonNode, Survey.class);
-        Long id = surveyRepository.save(survey).getId();
-        System.out.printf(id.toString());
-    }
-
-    @Order(1)
     @Test
+    @Transactional
+    @Sql(scripts = "/data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     @DisplayName("설문조사 수정 성공")
     void test_case_4() throws Exception {
-        // given
-        setup();
-
         // given
         String updateRequest = FileUtils.readFileAsString("testcase/survey_update_success.txt");
         // when // then
