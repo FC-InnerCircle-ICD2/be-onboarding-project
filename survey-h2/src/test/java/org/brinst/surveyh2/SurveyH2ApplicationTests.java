f8d147176dda28a8423166ad99d3b1352d66b2c5
brinst07 <brinst07@gmail.com>
2024-12-03 19:42:47 +0900

    [refactor] h2 모듈 분리

---
+++ survey-h2/src/test/java/org/brinst/surveyh2/SurveyH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.brinst.surveyh2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveyH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
