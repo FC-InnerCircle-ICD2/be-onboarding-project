5c4d7cab164414cfee51b4fbb3d5728948244247
minji <smj9030@gmail.com>
2024-12-04 22:25:16 +0900

    SurveyController 수정

--- src/main/java/com/onboarding/beonboardingproject/controller/SurveyController.java
+++ src/main/java/com/onboarding/beonboardingproject/controller/SurveyController.java
@@ -1,5 +1,6 @@
 package com.onboarding.beonboardingproject.controller;
 
+import com.onboarding.beonboardingproject.common.exception.InvalidException;
 import com.onboarding.beonboardingproject.common.global.ApiResult;
 import com.onboarding.beonboardingproject.common.global.ResponseCode;
 import com.onboarding.beonboardingproject.dto.ResponseDto;
@@ -22,7 +23,7 @@ public class SurveyController {
 
     // 설문 조사 생성
     @PostMapping
-    public ResponseEntity<ApiResult<SurveyResponseDto>> createSurvey(@RequestBody SurveyDto surveyDto) {
+    public ResponseEntity<ApiResult<String>> createSurvey(@RequestBody SurveyDto surveyDto) {
         SurveyResponseDto surveyResponseDto = surveyService.createSurvey(surveyDto);
 
         ApiResult apiResult = ApiResult.success("설문조사가 생성되었습니다. [Survey ID: " + surveyResponseDto.getSurveyId() + "]");
@@ -32,22 +33,15 @@ public class SurveyController {
 
     // 설문 조사 수정
     @PutMapping("/{surveyId}")
-    public ResponseEntity<Void> updateSurvey(@PathVariable Long surveyId, @RequestBody SurveyDto surveyDto) {
-        surveyService.updateSurvey(surveyId, surveyDto);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
-    }
+    public ResponseEntity<ApiResult<SurveyResponseDto>> updateSurvey(@PathVariable Long surveyId, @RequestBody SurveyDto surveyDto) {
+        ApiResult apiResult;
 
-    // 설문 응답 제출
-    @PostMapping("/{surveyId}/responses")
-    public ResponseEntity<Void> submitResponse(@RequestBody ResponseDto responseDto) {
-        surveyService.submitResponse(responseDto);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
-    }
-
-    // 설문 응답 조회
-    @GetMapping("/{surveyId}/responses")
-    public ResponseEntity<List<ResponseDto>> getResponses(@PathVariable Long surveyId) {
-        List<ResponseDto> responses = surveyService.getResponses(surveyId);
-        return ResponseEntity.ok(responses);
+        try {
+            SurveyResponseDto surveyResponseDto = surveyService.updateSurvey(surveyId, surveyDto);
+            apiResult = new ApiResult(ResponseCode.SUCCESS, surveyResponseDto);
+        }catch (InvalidException e){
+            apiResult = ApiResult.failure("설문조사 수정 실패하였습니다."+e.getMessage());
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(apiResult);
     }
 }
