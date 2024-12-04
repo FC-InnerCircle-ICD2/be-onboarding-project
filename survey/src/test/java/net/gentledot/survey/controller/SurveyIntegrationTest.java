a31b45f9f5d95a7c11995c48446d251ebd6f5134
gentledot <gentledot.wp@gmail.com>
2024-12-04 21:42:58 +0900

    fix(POINT-1): 통합테스트를 통해 로직상 오류가 있는 부분 추가 수정 - 응답 제출 및 응답 조회

--- survey/src/test/java/net/gentledot/survey/controller/SurveyIntegrationTest.java
+++ survey/src/test/java/net/gentledot/survey/controller/SurveyIntegrationTest.java
@@ -20,6 +20,7 @@ import net.gentledot.survey.repository.SurveyQuestionRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -200,28 +201,24 @@ class SurveyIntegrationTest {
 
     private static String testSurveyAnswerRequestBody() {
         return """
-                    [
-                         {
-                             "questionId": "1",
-                             "answer": "홍길동"
-                         },
-                         {
-                             "questionId": "2",
-                             "questionOptionId": "1"
-                         },
-                         {
-                             "questionId": "3",
-                             "questionOptionId": "3"
-                         },
-                         {
-                             "questionId": "3",
-                             "questionOptionId": "4"
-                         },
-                         {
-                             "questionId": "4",
-                             "answer": "아주 좋았습니다."
-                         }
-                     ]
+                [
+                    {
+                        "questionId": 1,
+                        "answer": ["홍길동"]
+                    },
+                    {
+                        "questionId": 2,
+                        "answer": ["좋아요"]
+                    },
+                    {
+                        "questionId": 3,
+                        "answer": ["23:00", "24:00"]
+                    },
+                    {
+                        "questionId": 4,
+                        "answer": ["아주 좋았습니다."]
+                    }
+                ]
                 """;
     }
 
@@ -251,6 +248,98 @@ class SurveyIntegrationTest {
                 .body("data.answerList[0].answers[2]", hasKey("answerValue"))
                 .body("data.answerList[0].answers[3]", hasKey("questionName"))
                 .body("data.answerList[0].answers[3]", hasKey("answerValue"))
+                .body("error", nullValue());
+    }
+
+    @DisplayName("설문 생성 - 설문 답변 제출 - 설문 수정 - 설문 답변 (2) - 설문 전체 조회를 쭉보자.")
+    @Test
+    void fullSurveyLifecycleTest() {
+        // Step 1: 서베이 생성
+        SurveyCreateRequest createRequest = testCreateRequestSimpleVersion();
+        String createRequestBody = toJson(createRequest);
+        ExtractableResponse<Response> createResponse = SurveyIntegrations.surveyCreate(createRequestBody)
+                .extract();
+        String surveyId = createResponse.path("data.surveyId");
+        Integer questionId1 = createResponse.path("data.questions[0].questionId");
+        Integer questionId2 = createResponse.path("data.questions[1].questionId");
+
+        // Step 2: 최초 응답 받기
+        String initialSubmitRequestBody = """
+                [
+                    {
+                        "questionId": %d,
+                        "answer": ["홍길동"]
+                    },
+                    {
+                        "questionId": %d,
+                        "answer": ["좋아요"]
+                    }
+                ]
+                """.formatted(questionId1, questionId2);
+
+        SurveyIntegrations.submitSurveyAnswer(surveyId, initialSubmitRequestBody)
+                .statusCode(HttpStatus.OK.value())
+                .body("success", equalTo(true))
+                .body("data", nullValue())
+                .body("error", nullValue());
+
+        // Step 3: 응답 수정 처리
+        SurveyUpdateRequest updateRequest = SurveyUpdateRequest.builder()
+                .id(surveyId)
+                .name("Updated Survey")
+                .description("Updated Description")
+                .questions(List.of(
+                        SurveyQuestionRequest.builder()
+                                .questionId(Long.valueOf(questionId1))
+                                .updateType(UpdateType.MODIFY)
+                                .question("Updated Question 1")
+                                .description("Updated Description 1")
+                                .type(SurveyItemType.SINGLE_SELECT)
+                                .required(ItemRequired.REQUIRED)
+                                .options(List.of(
+                                        new SurveyQuestionOptionRequest("Updated Option 1"),
+                                        new SurveyQuestionOptionRequest("Updated Option 2")
+                                ))
+                                .build(),
+                        SurveyQuestionRequest.builder()
+                                .questionId(Long.valueOf(questionId2))
+                                .updateType(UpdateType.DELETE)
+                                .build()
+                ))
+                .build();
+
+        String updateRequestBody = toJson(updateRequest);
+        SurveyIntegrations.surveyUpdate(updateRequestBody)
+                .statusCode(HttpStatus.OK.value())
+                .body("success", equalTo(true))
+                .body("data.surveyId", not(emptyString()))
+                .body("data.updatedAt", notNullValue())
+                .body("error", nullValue());
+
+        // Step 4: 응답 추가 조회
+        String updatedSubmitRequestBody = """
+                [
+                    {
+                        "questionId": %d,
+                        "answer": ["Updated Option 1"]
+                    }
+                ]
+                """.formatted(questionId1);
+
+        SurveyIntegrations.submitSurveyAnswer(surveyId, updatedSubmitRequestBody)
+                .statusCode(HttpStatus.OK.value())
+                .body("success", equalTo(true))
+                .body("data", nullValue())
+                .body("error", nullValue());
+
+        // Step 5: 전체 응답 조회 하기
+        SurveyIntegrations.getAllSurveyAnswers(surveyId, null, null)
+                .statusCode(HttpStatus.OK.value())
+                .body("success", equalTo(true))
+                .body("data.surveyId", equalTo(surveyId))
+                .body("data.answerList", hasSize(2))
+                .body("data.answerList[0].answers", hasSize(2))
+                .body("data.answerList[1].answers", hasSize(1))
                 .body("error", nullValue());
     }
 
@@ -305,6 +394,34 @@ class SurveyIntegrationTest {
                                 .required(ItemRequired.OPTIONAL)
                                 .options(List.of(
                                         new SurveyQuestionOptionRequest("입력 2")
+                                ))
+                                .build()
+                ))
+                .build();
+    }
+
+    private SurveyCreateRequest testCreateRequestSimpleVersion() {
+        return SurveyCreateRequest.builder()
+                .name("나만의 설문")
+                .description("간단한 설문입니다.")
+                .questions(List.of(
+                        SurveyQuestionRequest.builder()
+                                .question("이름을 알려주세요")
+                                .description("당신의 이름은 무엇입니까?")
+                                .type(SurveyItemType.TEXT)
+                                .required(ItemRequired.REQUIRED)
+                                .options(List.of(
+                                        new SurveyQuestionOptionRequest("입력 1")
+                                ))
+                                .build(),
+                        SurveyQuestionRequest.builder()
+                                .question("오늘의 기분을 알려주세요")
+                                .description("가벼운 질문부터")
+                                .type(SurveyItemType.SINGLE_SELECT)
+                                .required(ItemRequired.REQUIRED)
+                                .options(List.of(
+                                        new SurveyQuestionOptionRequest("좋아요"),
+                                        new SurveyQuestionOptionRequest("안좋아요")
                                 ))
                                 .build()
                 ))
