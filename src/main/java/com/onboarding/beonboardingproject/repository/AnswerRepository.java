41d5f5045c3b9b6d71cfff1b440ca2d07bdaefbc
minji <smj9030@gmail.com>
2024-12-04 22:25:56 +0900

    Repository 수정

--- src/main/java/com/onboarding/beonboardingproject/repository/AnswerRepository.java
+++ src/main/java/com/onboarding/beonboardingproject/repository/AnswerRepository.java
@@ -1,9 +1,13 @@
 package com.onboarding.beonboardingproject.repository;
 
 import com.onboarding.beonboardingproject.domain.response.Answer;
+import com.onboarding.beonboardingproject.domain.survey.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AnswerRepository extends JpaRepository<Answer, Integer> {
+    List<Answer> findByQuestion(Question question);
 }
--- src/main/java/com/onboarding/beonboardingproject/repository/OptionRepository.java
+++ src/main/java/com/onboarding/beonboardingproject/repository/OptionRepository.java
@@ -5,7 +5,11 @@ import com.onboarding.beonboardingproject.domain.survey.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OptionRepository extends JpaRepository<Option, Long> {
     void deleteByQuestion(Question question);
+
+    List<Option> findByQuestion(Question question);
 }
