bb207a5303f4acf82e0891d97ecb6d2e1c87efb5
yesjm <yesjm.dev@gmail.com>
2024-12-06 19:36:35 +0900

    feat: 설문조사 수정 api docs 추가

--- survey-api/src/test/kotlin/org/icd/surveyapi/surveyproducer/fixture/SurveyProducerFixture.kt
+++ survey-api/src/test/kotlin/org/icd/surveyapi/surveyproducer/fixture/SurveyProducerFixture.kt
@@ -1,7 +1,6 @@
 package org.icd.surveyapi.surveyproducer.fixture
 
-import org.icd.surveyapi.surveyproducer.application.dto.request.PostSurveyItemRequest
-import org.icd.surveyapi.surveyproducer.application.dto.request.PostSurveyRequest
+import org.icd.surveyapi.surveyproducer.application.dto.request.*
 import org.icd.surveyapi.surveyproducer.application.dto.response.*
 import org.icd.surveycore.domain.surveyItem.ItemType
 import java.time.OffsetDateTime
@@ -109,3 +108,39 @@ fun createGetSurveyResponse() = GetSurveyResponse(
         )
     )
 )
+
+fun createPatchSurveyRequest(): PatchSurveyRequest {
+    return PatchSurveyRequest(
+        name = "테스트 설문조사 수정",
+        description = "설문조사 수정 테스트",
+        deleteItems = listOf(1),
+        updateItems = listOf(
+            UpdateSurveyItemRequest(
+                id = 2,
+                sequence = 2,
+                name = "2번 항목 수정",
+                description = null,
+                itemType = ItemType.MULTIPLE_CHOICE,
+                deleteOptions = listOf(1),
+                updateOptions = listOf(
+                    UpdateSurveyItemOptionRequest(
+                        id = 2,
+                        name = "옵션명 변경"
+                    )
+                ),
+                createOptions = listOf("새로운 옵션"),
+                isRequired = true,
+            )
+        ),
+        createItems = listOf(
+            CreateSurveyItemRequest(
+                sequence = 3,
+                name = "새로운 항목",
+                description = "추가",
+                itemType = ItemType.SINGLE_CHOICE,
+                options = listOf("옵션1", "옵션2"),
+                isRequired = false
+            )
+        )
+    )
+}
--- survey-api/src/test/kotlin/org/icd/surveyapi/surveyproducer/presentation/SurveyProducerControllerTest.kt
+++ survey-api/src/test/kotlin/org/icd/surveyapi/surveyproducer/presentation/SurveyProducerControllerTest.kt
@@ -1,13 +1,12 @@
 package org.icd.surveyapi.surveyproducer.presentation
 
-import org.icd.surveyapi.exception.DuplicateSurveyItemSequenceException
-import org.icd.surveyapi.exception.MissingRequiredFieldException
-import org.icd.surveyapi.exception.NotFoundSurveyException
+import org.icd.surveyapi.exception.*
 import org.icd.surveyapi.support.BaseControllerTest
 import org.icd.surveyapi.support.docs.ExceptionSnippet
 import org.icd.surveyapi.surveyproducer.application.SurveyProducerService
 import org.icd.surveyapi.surveyproducer.application.dto.response.PostSurveyResponse
 import org.icd.surveyapi.surveyproducer.fixture.createGetSurveyResponse
+import org.icd.surveyapi.surveyproducer.fixture.createPatchSurveyRequest
 import org.icd.surveyapi.surveyproducer.fixture.createPostSurveyRequest
 import org.icd.surveycore.domain.surveyItem.ItemType
 import org.junit.jupiter.api.Test
@@ -20,8 +19,7 @@ import org.springframework.restdocs.payload.PayloadDocumentation.*
 import org.springframework.restdocs.request.RequestDocumentation.parameterWithName
 import org.springframework.restdocs.request.RequestDocumentation.pathParameters
 import org.springframework.test.context.bean.override.mockito.MockitoBean
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import java.time.OffsetDateTime
 
@@ -62,6 +60,7 @@ class SurveyProducerControllerTest : BaseControllerTest() {
                 ExceptionSnippet(
                     listOf(
                         MissingRequiredFieldException("필드명"),
+                        InvalidSurveyItemCountException(),
                         DuplicateSurveyItemSequenceException()
                     )
                 )
@@ -94,32 +93,96 @@ class SurveyProducerControllerTest : BaseControllerTest() {
                     fieldWithPath("items[].sequence").description("항목의 순서").type(JsonFieldType.NUMBER),
                     fieldWithPath("items[].name").description("항목의 이름").type(JsonFieldType.STRING),
                     fieldWithPath("items[].description").description("항목의 설명").optional().type(JsonFieldType.STRING),
-                    fieldWithPath("items[].itemType").description("항목 유형").type(generateEnumValues(ItemType::class.java)),
+                    fieldWithPath("items[].itemType").description("항목 유형")
+                        .type(generateEnumValues(ItemType::class.java)),
                     fieldWithPath("items[].options[]").description("선택지 옵션").optional().type(JsonFieldType.ARRAY),
-                    fieldWithPath("items[].options[].id").description("선택지 옵션 ID").optional().type(JsonFieldType.NUMBER),
-                    fieldWithPath("items[].options[].name").description("선택지 옵션 이름").optional().type(JsonFieldType.STRING),
+                    fieldWithPath("items[].options[].id").description("선택지 옵션 ID").optional()
+                        .type(JsonFieldType.NUMBER),
+                    fieldWithPath("items[].options[].name").description("선택지 옵션 이름").optional()
+                        .type(JsonFieldType.STRING),
                     fieldWithPath("items[].options[].isActive").description("옵션 활성화 여부").type(JsonFieldType.BOOLEAN),
                     fieldWithPath("items[].isRequired").description("항목 필수 응답 여부").type(JsonFieldType.BOOLEAN),
                     fieldWithPath("responses[]").description("응답 목록").optional().type(JsonFieldType.ARRAY),
                     fieldWithPath("responses[].id").description("응답의 ID").type(JsonFieldType.NUMBER),
-                    fieldWithPath("responses[].createdAt").description("응답 제출 일시").type(OffsetDateTime::class.simpleName),
+                    fieldWithPath("responses[].createdAt").description("응답 제출 일시")
+                        .type(OffsetDateTime::class.simpleName),
                     fieldWithPath("responses[].items[].sequence").description("항목 순서"),
                     fieldWithPath("responses[].items[].name").description("항목 이름"),
                     fieldWithPath("responses[].items[].description").description("항목 설명"),
                     fieldWithPath("responses[].items[].itemType").description("항목 유형"),
-                    fieldWithPath("responses[].items[].shortResponse").description("단답형 응답").optional().type(JsonFieldType.STRING),
-                    fieldWithPath("responses[].items[].longResponse").description("장문형 응답").optional().type(JsonFieldType.STRING),
+                    fieldWithPath("responses[].items[].shortResponse").description("단답형 응답").optional()
+                        .type(JsonFieldType.STRING),
+                    fieldWithPath("responses[].items[].longResponse").description("장문형 응답").optional()
+                        .type(JsonFieldType.STRING),
                     fieldWithPath("responses[].items[].singleChoiceResponse").description("단일 선택형 응답").optional(),
-                    fieldWithPath("responses[].items[].singleChoiceResponse.itemOptionId").description("옵션 ID").optional().type(JsonFieldType.NUMBER),
-                    fieldWithPath("responses[].items[].singleChoiceResponse.itemOptionName").description("옵션 이름").optional().type(JsonFieldType.STRING),
+                    fieldWithPath("responses[].items[].singleChoiceResponse.itemOptionId").description("옵션 ID")
+                        .optional().type(JsonFieldType.NUMBER),
+                    fieldWithPath("responses[].items[].singleChoiceResponse.itemOptionName").description("옵션 이름")
+                        .optional().type(JsonFieldType.STRING),
                     fieldWithPath("responses[].items[].multipleChoiceResponse").description("다중 선택형 응답").optional(),
-                    fieldWithPath("responses[].items[].multipleChoiceResponse[].itemOptionId").description("옵션 ID").optional().type(JsonFieldType.NUMBER),
-                    fieldWithPath("responses[].items[].multipleChoiceResponse[].itemOptionName").description("옵션 이름").optional().type(JsonFieldType.STRING),
-
-                    ),
+                    fieldWithPath("responses[].items[].multipleChoiceResponse[].itemOptionId").description("옵션 ID")
+                        .optional().type(JsonFieldType.NUMBER),
+                    fieldWithPath("responses[].items[].multipleChoiceResponse[].itemOptionName").description("옵션 이름")
+                        .optional().type(JsonFieldType.STRING),
+                ),
                 ExceptionSnippet(
                     listOf(
                         NotFoundSurveyException()
+                    )
+                )
+            ),
+        ).andExpect(status().isOk)
+    }
+
+    @Test
+    fun patchSurvey() {
+        val request = createPatchSurveyRequest()
+
+        given { surveyProducerService.patchSurvey(any(), any()) }.willReturn("OK")
+
+        mockMvc.perform(
+            patch("/v1/survey/{surveyId}", 1)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request))
+                .accept(MediaType.APPLICATION_JSON),
+        ).andDo(
+            document(
+                "patchSurvey",
+                pathParameters(
+                    parameterWithName("surveyId").description("설문조사 ID")
+                ),
+                requestFields(
+                    fieldWithPath("name").description("설문조사의 이름"),
+                    fieldWithPath("description").description("설문조사의 설명").optional(),
+                    fieldWithPath("deleteItems").description("삭제할 항목 ID 리스트").optional(),
+                    fieldWithPath("updateItems").description("수정할 항목 리스트").optional(),
+                    fieldWithPath("updateItems[].id").description("수정할 항목 ID"),
+                    fieldWithPath("updateItems[].sequence").description("수정된 항목의 순서"),
+                    fieldWithPath("updateItems[].name").description("수정된 항목의 이름"),
+                    fieldWithPath("updateItems[].description").description("수정된 항목의 설명").optional(),
+                    fieldWithPath("updateItems[].itemType").description("수정된 항목의 타입"),
+                    fieldWithPath("updateItems[].deleteOptions").description("삭제할 항목 옵션 ID 리스트").optional(),
+                    fieldWithPath("updateItems[].updateOptions").description("수정할 옵션 리스트").optional(),
+                    fieldWithPath("updateItems[].updateOptions[].id").description("수정할 옵션 ID"),
+                    fieldWithPath("updateItems[].updateOptions[].name").description("수정된 옵션의 이름"),
+                    fieldWithPath("updateItems[].createOptions").description("새로 추가할 옵션 리스트").optional(),
+                    fieldWithPath("updateItems[].isRequired").description("필수 여부"),
+                    fieldWithPath("createItems").description("새로 생성할 항목 리스트").optional(),
+                    fieldWithPath("createItems[].sequence").description("새로 생성할 항목의 순서"),
+                    fieldWithPath("createItems[].name").description("새로 생성할 항목의 이름"),
+                    fieldWithPath("createItems[].description").description("새로 생성할 항목의 설명").optional(),
+                    fieldWithPath("createItems[].itemType").description("새로 생성할 항목의 타입"),
+                    fieldWithPath("createItems[].options").description("새로 생성할 항목의 옵션 리스트").optional(),
+                    fieldWithPath("createItems[].isRequired").description("필수 여부")
+                ),
+                ExceptionSnippet(
+                    listOf(
+                        MissingRequiredFieldException("필드명"),
+                        NotFoundSurveyException(),
+                        NotFoundSurveyItemException(),
+                        NotFoundSurveyItemOptionException(),
+                        InvalidSurveyItemCountException(),
+                        DuplicateSurveyItemSequenceException()
                     )
                 )
             ),
